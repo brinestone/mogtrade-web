a8df66595b4d0f0996de65ac7478a7f34714af5e
Conrad Bekondo <conradbekondo@gmail.com>
2026-09-05 21:28:59 +0100

    feat: enhance application configuration for server-side rendering and hydration
    - Updated app.config.ts to include client hydration and HTTP client providers.
    - Added preconnect links for Google Fonts in index.html.
    - Modified tsconfig.app.json to include Node types.
    - Cleaned up tsconfig.json by removing unused references.
    - Introduced app.config.server.ts for server-side rendering configuration.
    - Created app.routes.server.ts to define server routes with prerendering.
    - Implemented main.server.ts to bootstrap the Angular application on the server.
    - Developed server.ts to handle requests and serve the Angular application using Express.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideClientHydration, withHttpTransferCacheOptions } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideClientHydration(withHttpTransferCacheOptions({
+      includePostRequests: false,
+    })),
+    provideHttpClient()
+  ],
 };
